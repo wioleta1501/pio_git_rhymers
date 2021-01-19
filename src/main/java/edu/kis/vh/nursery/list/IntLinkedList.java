fcc43f8bbf8d5f134769fc3e21ef177f2b8053a4
wioleta1501 <wioleta1501@gmail.com>
2021-01-19 21:10:48 +0100

    Classes and methods refactor

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -9,9 +9,9 @@ public class IntLinkedList {
 		if (last == null)
 			last = new Node(i);
 		else {
-			last.next = new Node(i);
-			last.next.prev = last;
-			last = last.next;
+			last.setNext(new Node(i));
+			last.getNext().setPrev(last);
+			last = last.getNext();
 		}
 	}
 
@@ -26,14 +26,14 @@ public class IntLinkedList {
 	public int top() {
 		if (isEmpty())
 			return -1;
-		return last.value;
+		return last.getValue();
 	}
 
 	public int pop() {
 		if (isEmpty())
 			return -1;
-		int ret = last.value;
-		last = last.prev;
+		int ret = last.getValue();
+		last = last.getPrev();
 		return ret;
 	}
 
